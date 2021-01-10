cb3f83fe0116b112e77cdb0c7a283fe5525cd5b9
liyanjun <254948133@qq.com>
2021-01-11 07:12:12 +0800

    增加二叉树

--- node/bst/bst.js
+++ node/bst/bst.js
@@ -8,9 +8,10 @@ class Node {
 }
 
 class Bst {
-  constructor() {
+  constructor(compare) {
     this.root = null;
     this.size = 0;
+    this.compare = compare || this.compare;
   }
   add (element) {
     if (!this.root) {
@@ -22,7 +23,7 @@ class Bst {
       // 获取节点，直到获取到无法比较大小， 当前元素大于比较节点，则取右侧查找，否则去左侧
       // 直到currentNode不存在，可以确定位置
       while (currentNode) {
-        compare = this.getVisited(element - currentNode.element);
+        compare = this.compare(element, currentNode.element);
         parent = currentNode;
         if (compare > 0) {
           currentNode = currentNode.right;
@@ -30,16 +31,81 @@ class Bst {
           currentNode = currentNode.left;
         } else {
           currentNode.element = element;
+          return;
         }
       }
+      const newNode = new Node(element, parent);
       if (compare > 0) {
-        parent.right = new Node(element, parent);
+        parent.right = newNode;
       } else if (compare < 0) {
-        parent.left = new Node(element, parent);
+        parent.left = newNode;
       }
     }
     this.size++;
     return this.root;
+  }
+  // 前序遍历
+  preorderTraversal(visitor){
+    const traversal = (node) => {
+      if(node == null || visitor == null){return;}
+      visitor(node);
+      traversal(node.left);
+      traversal(node.right);
+    }
+    traversal(this.root);
+  }
+  // 中序遍历
+  inorderTraversal(visitor){
+    const traversal = (node) => {
+      if(node == null || visitor == null){return;}
+      traversal(node.left);
+      visitor(node);
+      traversal(node.right);
+    }
+    traversal(this.root);
+  }
+  // 后续遍历
+  postorderTraversal(visitor){
+    const traversal = (node) => {
+      if(node == null || visitor == null){return;}
+      traversal(node.left);
+      traversal(node.right);
+      visitor(node);
+    }
+    traversal(this.root);
+  }
+  // 层序遍历
+  levelorderTraversal(visitor){
+    if(this.root == null || visitor == null){return;}
+    const stack = [this.root];
+    let index = 0;
+    let currentNode = null;
+    while( currentNode = stack[index++]) {
+      visitor(currentNode);
+      if(currentNode.left) {
+        stack.push(currentNode.left)
+      }
+      if(currentNode.right) {
+        stack.push(currentNode.right)
+      }
+    }
+  }
+  invertTree(){
+    if(this.root == null){return;}
+    const stack = [this.root];
+    let current;
+    let index = 0;
+    while(current= stack[index++]){
+      const tem = current.left;
+      current.left = current.right;
+      current.right = tem;
+      if(current.left) {
+        stack.push(current.left)
+      }
+      if(current.right) {
+        stack.push(current.right)
+      }
+    }
   }
   remove () {
     this.size--;
@@ -47,13 +113,26 @@ class Bst {
   contain () {
 
   }
-  getVisited (a, b) {
+  compare (a, b) {
     return a - b;
   }
 }
 const bst = new Bst();
-let arr = [10, 8, 19, 6, 15, 22];
+let arr = [10, 8, 19, 6, 15, 22,20];
 arr.forEach(item => {
   bst.add(item);
 });
-console.log(bst.root)
+bst.invertTree();
+bst.preorderTraversal((node) => {
+  console.log(node.element)
+})
+// bst.inorderTraversal((node) => {
+//     console.log(node.element)
+//   });
+// bst.postorderTraversal((node) => {
+//   console.log(node.element)
+// })
+// bst.levelorderTraversal((node) => {
+//   console.log(node.element)
+// })
+// console.dir(, {depth:200})
--- node/buffer/buffer.js
+++ node/buffer/buffer.js
@@ -3,10 +3,30 @@ let buf1 = Buffer.alloc(9);
 let buf2 = Buffer.from("艳军")
 let buf3 =Buffer.from([97,98,99])
 
-buf2.copy(buf1)
-console.log(buf1.byteLength)
-buf3.copy(buf1,6,0,1)
-console.log(buf1.toString())
-Buffer.prototype.myCopy = function(target,targetStart,sourceStart, sourceEnd){
+// buf2.copy(buf1)
+// console.log(buf1.byteLength)
+// buf3.copy(buf1,6,0,1)
+// console.log(buf1.toString())
+// Buffer.prototype.myCopy = function(target,targetStart=0,sourceStart=0, sourceEnd=this.length){
+//     for(let i=sourceStart; i<sourceEnd;i++){    
+//         target[targetStart++] = this[i];
+//     }
+// }
+// buf2.myCopy(buf1)
+// console.log(buf1.toString())
+// buf3.myCopy(buf1,6,0,1)
+// console.log(buf1.toString())
+console.log(Buffer.concat([buf2,buf3]).toString())
 
+Buffer.myConcat = function(bufList,len=bufList.reduce((prev,cur)=> prev+cur.length,0)){
+    const targetBuf = Buffer.alloc(len);
+    let offset = 0;
+    bufList.forEach(buffer => {
+        for(let i=0; i<buffer.length; i++){
+            targetBuf[offset++] = buffer[i];
+        }
+    });
+    return targetBuf;
 }
+console.log(Buffer.myConcat([buf2,buf3]).toString())
+

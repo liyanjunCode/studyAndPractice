d0806d59fc44079f6be572065d6e7cbd60e185c1
liyanjun <254948133@qq.com>
2021-01-22 06:43:57 +0800

    gzip

--- node/fs/index1.js
+++ node/fs/index1.js
@@ -1,17 +1,25 @@
 const path = require("path");
+const { setTimeout } = require("timers");
 const {createReadStream} = require("./createReadStream.js");
 const fsWrite = createReadStream(path.resolve(__dirname,"./1.text"), {
   flags:"r",
   encoding: "utf8",
   mode: 0o666,
   autoClose: true,
-  highWaterMark: 1,
+  highWaterMark: 5,
 });
 fsWrite.on("open", function(fd){
   console.log(fd, "open")
 })
+let num =true;
 fsWrite.on("data",function(chunk) {
-  console.log(chunk)
+  console.log(chunk.toString(),"111")
+  if(num) {
+    num = false;
+    fsWrite.pause();
+    
+  }
+  
 })
 fsWrite.on("end",function(fd) {
   console.log(fd, "end")
@@ -19,3 +27,7 @@ fsWrite.on("end",function(fd) {
 fsWrite.on("close",function(fd) {
   console.log(fd, "关闭")
 })
+setTimeout(()=>{
+  console.log("恢复--------------------------------------")
+  fsWrite.resume()
+}, 1000)
--- node/serverjs/src/server.js
+++ node/serverjs/src/server.js
@@ -1,16 +1,21 @@
 const http = require('http');
-const fs = require("fs").promises;
+const fs = require("fs/promises");
+const { createReadStream,readFileSync } = require("fs");
+const path = require("path");
 const mime = require("mime");
 const { program } = require("commander");
 const chalk = require("chalk");
 const url = require("url");
+const ejs = require("ejs");
+const util = require("util");
+const zlib = require("zlib");
 program.version('1.0.0');
 program.option('-p, --port <type>')
     .option('-h, --host <type>')
     .option('-d, --dir <type>');
 program.parse(process.args);
 const options = program.opts();
-console.log(URL, URLSearchParams, 88888);
+const template = readFileSync(path.join(__dirname, "./public.ejs"));
 class Serve {
     constructor(config) {
         this.port = config.port || 3000;
@@ -18,16 +23,61 @@ class Serve {
         this.dirPath = config.dirPath
     }
     async handleRes (req, res) {
-        const urls = url.parse(req.url, true);
-        let absFilePath = __dirname + urls.pathname;
-        const status = await fs.stat(path);
-        if (status.isDirectory()) {
-            absFilePath = path.join(absFilePath, "index.html")
+        const {pathname} = url.parse(req.url, true);
+        let absFilePath=path.join(__dirname, pathname);
+        console.log(absFilePath, "absFilePath")
+        try {
+            const status = await fs.stat(absFilePath);
+            if (status.isFile()) {
+                this.sendFile(req, res, status, absFilePath)
+            } else {
+                const filePath = path.join(absFilePath, "index.html");
+                try {
+                    const status = await fs.stat(filePath);
+                    this.sendFile(req, res, status, filePath);
+                }catch(err){
+                    this.showList(req, res, status, absFilePath);
+                }
+            }
+        }catch(err){
+            this.sendError(req, res, absFilePath);
+           
         }
-        this.sendFile(req, res, status, absFilePath)
+    }
+    async showList(req, res, status, absFilePath){
+        const dirlist = await fs.readdir(absFilePath);
+        let dirs = dirlist.map(item=>{
+            return {
+                name: item,
+                path: `/public/${item}`
+            }
+        })
+        let templateStr = await ejs.render(template, { dirs }, { async:true });
+       
+        res.end(templateStr);
+    }
+    sendError(req, res, absFilePath){
+        res.statusCode = 404;
+        res.end("not Found");
+    }
+    zgip(req, res){
+        if(req.headers['accept-encoding'] && req.headers['accept-encoding'].includes("gzip")) {
+            res.setHeader("Content-Encoding", "gzip");
+            return zlib.createGzip();
+        }
+        return false;
     }
     sendFile (req, res, status, absFilePath) {
-        res.end("chenggon")
+        res.statusCode = 200;
+        const type = mime.getType(absFilePath);
+        res.setHeader("Content-Type",type+";charset=utf-8");
+        const zgips = this.zgip(req, res);
+        if(zgips) {
+            createReadStream(absFilePath).pipe(zgips).pipe(res);
+        } else {
+            createReadStream(absFilePath).pipe(res);
+        }
+        
     }
     start () {
         const server = http.createServer(this.handleRes.bind(this))
